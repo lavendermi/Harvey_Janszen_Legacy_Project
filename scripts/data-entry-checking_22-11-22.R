b59b21addae732f273260d230ddffb5069fb680f
emench <emcanucks@gmail.com>
2022-12-01 14:16:02 -0800

    updating processing part I script, adjusting cocurrence entry template, and renaming occurrence entry files in new more specific folders

--- scripts/data-entry-checking_22-11-22.R
+++ scripts/data-entry-checking_22-11-22.R
@@ -24,13 +24,39 @@ rm(requiredPackages)
 
 ## READING IN DATA ----
   data <- read_excel(
-    here::here("data","digitized_data",
-               "HJ-occ-entry-template_22-11-11.xlsx"))
+    here::here("data","digitized_data", "raw_data",
+               "HJ-7-occ-entry.xlsx"))
 
-## 1) check where there are dataEntryRemarks, keep these columns
+  # renaming column names for easier recognition 
+  data <- data %>%dplyr::rename(archiveID="[archiveID]", pageNum = "[pageNum]", 
+                                numPage = "[numPage]", vName = "[vName]",
+                                vSciName= "[vSciName]", sciName = "[sciName]",
+                                conf="[conf]",date="[date]",
+                                locality="[locality]", country = "[country]",
+                                stateProvince = "[stateProvince]", 
+                                island ="[island]")
 
-## 2) Check where there is missing sciName and vSci Name
+## 1. Extract rows where... ----
+# a) vName, vSciName, or sciName are empty
+# b) there are dataEntry remarks
+# c) there is no date
+# d) there is no locality, country, stateProvince
+# e) rows with low confidence in taxon name deciphering
 
-## 3) Check where there is low confidence in taxon name 
+to_check_high_priority <- data %>% 
+  dplyr::filter(is.na(vName) | is.na(vSciName) | is.na(sciName) | 
+                  !is.na(dataEntryRemarks) |
+                  is.na(date)| 
+                  is.na(locality) | conf=="l") %>% 
+    dplyr::filter(., rowSums(is.na(.)) != ncol(.)) # removing rows that contain all NA values
 
-## 4) If time, places where medium confidence in taxon name 
+  ## saving file 
+  write.csv(to_check_high_priority, here::here("data","digitized_data","data_checking", paste0("occ-data-to-check_HIGH-PRIORITY_",
+                                                                Sys.Date(),".csv")))
+## If time add rows where there is medium confidence in taxon name deciphering
+
+  to_check <- rbind(to_check_high_priority, data[data$conf=="m",]) 
+  to_check <- to_check %>% dplyr::filter(., rowSums(is.na(.)) != ncol(.))
+
+  ## saving file 
+  write.csv(to_check, here::here("data","digitized_data","data_checking", paste0("occ-data-to-check_ALL_",Sys.Date(),".csv")))
