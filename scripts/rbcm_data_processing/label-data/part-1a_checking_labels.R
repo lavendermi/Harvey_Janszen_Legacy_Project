ff8afb0f0aacb32a56c8b9ea23bd73a93e331945
emench <emcanucks@gmail.com>
2023-01-29 10:53:49 -0800

    fixing large image file commit

---
+++ scripts/rbcm_data_processing/label-data/part-1a_checking_labels.R
@@ -0,0 +1,170 @@
+###########################################################
+#######             Processing step Ia.             #######
+#######       Checking entered collection data      #######
+#######           Emma Menchions 22-11-22           #######                       
+###########################################################
+
+# TO DO: 
+# with labels ...
+# Add V to front of accession number
+
+# The goal of this script is to....
+
+# 1. identify occurrence rows flagged by individual during 
+# data entry either by:
+# a) remarking something in the dataEntryRemarks
+# b) Not entering all taxon name information
+# c) low confidence in taxon name interpretation 
+# d) uncertainty in location and and no locality entered
+
+# 2. Create a new spreadsheet of these collections so they 
+# this uncertainty can be checked one row at a time
+
+## LOADING PACKAGES ----
+library(groundhog)
+
+set.groundhog.folder(here::here("packages"))
+date <- "2022-11-02"
+requiredPackages <-  c("assertr","readxl","dplyr","here", "tidyverse","tidyr")
+
+for (pkg in requiredPackages) {
+  groundhog.library(pkg, date)
+}
+
+rm(requiredPackages)
+
+## 1. LOADING IN RAW DATA ----
+
+  J <-7 # Journal number (only ONE at a time) --> USER INPUT !
+
+  total_data <- read_excel(here::here("data","data_digitization", 
+          "collection_data","1_raw_data", 
+          paste0("HJ-",J,"-","coll-entry.xlsx"))) %>% 
+  dplyr::rename(pageNum = "[pageNum]", 
+  numPage = "[numPage]", 
+  recordNum= "[recordNum]",
+  vName = "[vName]",
+  vSciName= "[vSciName]", 
+  sciName = "[sciName]",
+  conf="[conf]",date="[date]",
+  locality="[locality]", 
+  country = "[country]",
+  stateProvince = "[stateProvince]", 
+  island ="[island]")
+  
+  ## finding new rows entered 
+  
+  # if there is one or more files that have been previously processed...
+  if(length(list.files(here::here("data", "data_digitization",
+                                  "collection_data",
+                                  "prev_processed","template_format", 
+                                  paste0("HJ",J))))>=1){
+    
+    # read in the file with the latest date (with most observations) 
+    old_data <- read.csv(here::here("data", "data_digitization","collection_data",
+                                    "prev_processed","template_format", paste0("HJ",J), unique(as.character(max(list.files(
+      here::here("data", "data_digitization","collection_data",
+                 "prev_processed","template_format", paste0("HJ",J)))))))) 
+
+    # remove rows from current data table that contain old data                                              
+    new_data <- total_data %>% anti_join(old_data)
+    
+  } else {
+    new_data <- total_data
+  }
+  
+  ## writing sheet of total columns reviewed/ processed
+  write.csv(total_data, here::here("data", "data_digitization","collection_data",
+                                 "prev_processed", "template_format",paste0("HJ",J), 
+                                 paste0("HJ-", J, "_rows-reviewed_",Sys.Date(),".csv")), row.names=F)
+  
+  
+## 2. EXTRACTING ROWS & WRITING SHEET FOR CHECKING ----
+  
+# Extract rows where... 
+# a) vName, vSciName, or sciName are empty
+# b) there are dataEntry remarks
+# c) there is no date
+# d) there is no locality, country, stateProvince
+# e) rows with lower confidence in taxon name deciphering
+
+  Q <- "hp" # "all" = will also include rows with 
+            # medium confidence in taxon name decipher
+  
+             # "hp" = high priority - will only write file 
+             # with these conditions and taxon 
+             # name with LOW confidence only
+  
+  if (Q == "hp"){
+
+    to_check <- new_data %>% 
+      dplyr::filter(is.na(vName) | is.na(vSciName) | is.na(sciName) | 
+                      !is.na(dataEntryRemarks) |
+                      is.na(date)| 
+                      is.na(locality) | conf=="l") %>% 
+      # removing rows that contain all NA values
+        dplyr::filter(., rowSums(is.na(.)) != ncol(.)) %>%  
+      # adding new column to which can be used to keep track of 
+      # data checking progress
+        mutate("checkStatus"= "", 
+               .before=pageNum) %>% 
+        mutate("toDelete"= "", # adding column to indicate which entries
+           # too poor or uncertain to include
+           .before=pageNum) %>% 
+        relocate(., dataEntryRemarks, .after= toDelete)
+    
+    
+      ## saving file 
+      write.csv(to_check, 
+                here::here("data","data_digitization","collection_data",
+                "2_data_checking", paste0("HJ",J),
+                 paste0("HJ-",J,"_","coll-data-to-check_HIGH-PRIORITY_",
+                 Sys.Date(),".csv")), row.names = F)
+      
+    } else if (Q=="all"){
+      
+      to_check <- new_data %>% 
+        dplyr::filter(is.na(vName) | is.na(vSciName) |
+                        is.na(sciName) | 
+                        !is.na(dataEntryRemarks) |
+                        is.na(date)| 
+                        is.na(locality) | conf=="l" | 
+                        conf == "m") %>% 
+        # removing rows that contain all NA values
+        dplyr::filter(., rowSums(is.na(.)) != ncol(.)) %>%  
+        # adding new column to which can be used to keep 
+        # track of data checking progress
+        mutate("checkStatus"= "",
+               .before=pageNum) %>% 
+        mutate("toDelete"= "", # adding column to indicate which entries
+               # too poor or uncertain to include
+             .before=pageNum) %>% 
+       relocate(., dataEntryRemarks, .after= toDelete)
+        
+      
+      ## saving file 
+      write.csv(to_check, 
+                here::here("data","data_digitization","collection_data",
+                           "2_data_checking", paste0("HJ",J),
+                           paste0("HJ-",J,"_","coll-data-to-check_ALL_",
+                           Sys.Date(),".csv")), row.names = F)
+    } 
+  
+  # and removing old files to save storage
+  if(length(list.files(here::here("data", 
+                                  "data_digitization","collection_data",
+                                  "2_data_checking", paste0("HJ",J))))>4){
+    file.remove(unique(as.character(min(list.files(here::here("data", 
+                                                              "data_digitization","collection_data",
+                                                              "2_data_checking", paste0("HJ",J)))))))   
+  }
+  
+## 3. ADDRESS THE ISSUES IN THESE ROWS in CSV FILE ----
+  # add data or change that data in these rows within the new
+  # .csv file created
+  # once they are all addressed... use script part1-b_checking 
+  # to consolidate and remove rows that can't be filled 
+  # in with all of the minimum required data
+  
+
+  
---
+++ scripts/rbcm_data_processing/label-data/part-1b_checking_labels.R
@@ -0,0 +1,139 @@
+###########################################################
+#######             Processing step Ib.             #######
+#######       Consolidating checked and raw data    #######
+#######           Emma Menchions 22-11-22           #######                       
+###########################################################
+
+## LOADING PACKAGES ----
+library(groundhog)
+
+set.groundhog.folder(here::here("packages"))
+date <- "2022-11-02"
+requiredPackages <-  c("assertr","expss", "readxl","dplyr",
+                       "here", "tidyverse","tidyr")
+
+
+for (pkg in requiredPackages) {
+  groundhog.library(pkg, date)
+}
+
+rm(requiredPackages)
+
+## 1. LOADING DATA ----
+
+  # USER INPUT: 
+
+  J <-7 # journal number (only ONE at a time) (USER INPUT) ! 
+  
+  # raw data
+  raw_data <- read_excel(here::here("data","data_digitization",
+                                    "collection_data",
+                                    "1_raw_data", paste0("HJ-",J,
+                                      "-","coll-entry.xlsx"))) %>% 
+   dplyr::rename(
+      pageNum = "[pageNum]", 
+      numPage = "[numPage]", 
+      recordNum = "[recordNum]",
+      vName = "[vName]",
+      vSciName= "[vSciName]", 
+      sciName = "[sciName]",
+      conf="[conf]",date="[date]",
+      locality="[locality]", 
+      country = "[country]",
+      stateProvince = "[stateProvince]", 
+      island ="[island]")
+  
+  # if there is one or more files that have been previously processed...
+  if(length(list.files(here::here("data", "data_digitization",
+                                  "collection_data",
+                                  "prev_processed","template_format", 
+                                  paste0("HJ",J))))>=1){
+    
+    # read in the file with the latest date (with most observations) 
+    old_data <- read.csv(here::here("data", "data_digitization","collection_data",
+                                    "prev_processed","template_format", paste0("HJ",J), unique(as.character(max(list.files(
+                                      here::here("data", "data_digitization","collection_data",
+                                                 "prev_processed","template_format", paste0("HJ",J)))))))) 
+    
+    # remove rows from current data table that contain old data                                              
+    raw_data <- raw_data %>% anti_join(old_data) %>% relocate(., dataEntryRemarks, .before= pageNum)
+    
+    
+  } 
+  
+  # most recent checked data entry
+    checked_data <- read.csv(
+    here::here("data","data_digitization",
+               "collection_data","2_data_checking", 
+               paste0("HJ",J), 
+               as.character(unique(max(list.files(here::here("data","data_digitization",
+                                                             "collection_data",
+                                                             "2_data_checking", paste0("HJ",J))))))))
+    # reading recordNumber as character: 
+    checked_data$recordNum <- as.character(checked_data$recordNum)
+  
+  
+## 2. HAVE ALL ROWS IN CHECK DATA BEEN REVIEWED? ----
+  checked_data %>% 
+  assert(in_set("C", "R", "c","r"), checkStatus) 
+  
+## 3. CONSOLIDATNG & REMOVING ROWS ----
+  
+  # removing rows which are empty
+  raw_data <- raw_data %>%  
+    dplyr::filter(!is.na(vName) & 
+    !is.na(vSciName) &
+    !is.na(sciName)) %>% 
+    dplyr::filter(., rowSums(is.na(.)) != ncol(.)) 
+  
+  checked_data <- checked_data %>% 
+    dplyr::filter(!is.na(vName) & 
+                    !is.na(vSciName) &
+                    !is.na(sciName)) %>% 
+    dplyr::filter(., rowSums(is.na(.)) != ncol(.)) 
+  
+  for (i in 1:dim(checked_data)[1]){  # for each row in the checked frame
+      if (checked_data$toDelete[i] == "Y" | checked_data$toDelete[i] == "y"){
+        for (j in 1:dim(raw_data)[1]){
+          if(checked_data$vName[i] == raw_data$vName[j]){
+              raw_data[j,] <- NA
+              raw_data[j,"vName"] <- "missing"
+              
+              
+          }
+        }
+      } else {
+      raw_data[ # if its archiveID, pageNum and numPage match that in raw data
+      which(checked_data$pageNum[i] == raw_data$pageNum &
+      checked_data$numPage[i] == raw_data$numPage),] <- 
+        # assign raw data column to that row of checked data
+        checked_data[i,3:dim(checked_data)[2]]
+    }
+  }
+  
+  raw_data[raw_data$vName=="missing","vName"] <- NA
+  
+## 4. WRITING CHECKED FILE FOR NEXT PROCESSING STEP ----
+  
+  processed_data_1 <- raw_data %>% # removing rows that contain all NA values
+                      relocate(., dataEntryRemarks, .after= idBy) %>% 
+                    dplyr::filter(., rowSums(is.na(.)) != ncol(.)) 
+  
+  # saving to data_cleaning folder to prepare for Processing step 2 (cleaning)
+  write.csv(processed_data_1, 
+            here::here("data", "data_digitization", 
+            "collection_data","3_data_cleaning",paste0("HJ",J),
+            paste0("HJ", J, "-processed-step-1_",Sys.Date(),".csv")),
+            row.names = F)
+  
+  # and removing old files to save storage
+  if(length(list.files(here::here("data", 
+                                  "data_digitization","collection_data",
+                                  "3_data_cleaning", paste0("HJ",J))))>2){
+    file.remove(unique(as.character(min(list.files(here::here("data", 
+                                                              "data_digitization","collection_data",
+                                                              "3_data_cleaning", paste0("HJ",J)))))))   
+  }
+            
+            
+  
